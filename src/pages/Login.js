9efcba038802ead4f9a429893abc0dc10ad16449
chaiqou <lomtadzenikusha@gmail.com>
2022-02-27 13:54:41 +0400

    configure tailwind.config.js , implement new font from google fonts , added background video full size on Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import LoginVideo from "../images/background.mp4";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -17,7 +18,26 @@ const Login = () => {
     document.title = "Login - Talgagram";
   }, []);
 
-  return <div className=" text-2xl text-blue-600">Login</div>;
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={LoginVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-indigo">
+          <div className="shadow-2xl">
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
