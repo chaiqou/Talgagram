dbbc1dc91e403ef6a85012ff6396355dd62b7669
chaiqou <lomtadzenikusha@gmail.com>
2022-03-20 23:58:24 +0400

    Conditionally render logic in header if user exists shows logout if user doesnt exists shows login and sign up buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import UserContext from "../context/user";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../constants/routes";
 import { FcHome } from "react-icons/fc";
+import { MdExitToApp } from "react-icons/md";
 
 const Header = () => {
   const { firebase } = useContext(FirebaseContext);
@@ -16,9 +17,32 @@ const Header = () => {
           <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
             <h1 className="flex justify-center w-full">
               <Link to={ROUTES.DASHBOARD}>
-                <FcHome className="w-full h-8" />
+                <FcHome className="w-full h-8 " />
               </Link>
             </h1>
+          </div>
+          <div className="text-grey-700 flex items-center text-center align-items">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBOARD}></Link>
+                <button
+                  type="button"
+                  title="Sign Out"
+                  onClick={() => firebase.auth().signOut()}
+                >
+                  <MdExitToApp className="w-full h-8 text-blue-900" />
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to={ROUTES.LOGIN}>
+                  <button>Log In</button>
+                </Link>
+                <Link to={ROUTES.SIGN_UP}>
+                  <button>SIgn Up</button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
