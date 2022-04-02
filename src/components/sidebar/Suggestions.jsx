2b386f7f7ab6f9d6de2e3c1e39c77cfce1ee5cac
chaiqou <lomtadzenikusha@gmail.com>
2022-04-02 12:07:42 +0400

    Created suggestions page and conditionally render Skeleton if users array === 0 , if > 0 then render another div

--- src/components/sidebar/Suggestions.jsx
+++ src/components/sidebar/Suggestions.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
-const Suggestions = () => {
-  return <div>Suggestions</div>;
+const Suggestions = ({ userId }) => {
+  const [profiles, setProfiles] = useState(null);
+
+  // nested ternary operator
+  return !profiles ? (
+    <Skeleton count={1} height={150} className="mt-5" />
+  ) : profiles.length > 0 ? (
+    <div className="flex flex-col rounded">
+      <div className="text-sm flex items-center justify-between align-items mb-2"></div>
+      <p className="text-bold text-gray-base">Suggestions for you</p>
+    </div>
+  ) : null;
 };
 
 export default Suggestions;
